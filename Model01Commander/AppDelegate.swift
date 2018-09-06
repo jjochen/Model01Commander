0304fcbca44988f458815671dd2821d84cf80988
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2018-09-06 13:38:26 +0200

    Initial Commit

---
+++ Model01Commander/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Model01Commander
+//
+//  Created by Jochen on 06.09.18.
+//  Copyright © 2018 Jochen Pfeiffer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Model01Commander/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Model01Commander
+//
+//  Created by Jochen on 06.09.18.
+//  Copyright © 2018 Jochen Pfeiffer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
