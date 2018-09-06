c03e4155276f17fb8976c29b1dc7ae599db33621
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2018-09-06 14:33:19 +0200

    setup menu bar status item

--- Model01Commander/AppDelegate.swift
+++ Model01Commander/AppDelegate.swift
@@ -9,18 +9,35 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+
+class AppDelegate: NSObject {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+}
 
 
-
+extension AppDelegate: NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        setupStatusItem()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
 }
 
+fileprivate extension AppDelegate {
+    func setupStatusItem() {
+        statusItem.button?.image = #imageLiteral(resourceName: "menu_bar_icon_16pt")
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Preferences...", action: #selector(AppDelegate.settingsMenuItemClicked(_:)), keyEquivalent: ","))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Model01 Commander", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+}
+
+fileprivate extension AppDelegate {
+    @objc func settingsMenuItemClicked(_ sender: Any?) {
+        print("settings menu item clicked")
+    }
+}
